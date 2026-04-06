18dded7ec25ecbda8e65eea2c393bad74f07a2f1
quan2407 <quannm24072003@gmail.com>
2026-04-06 10:07:54 +0700

    btvn session 06

---
+++ session-06/carparkmanagement/src/main/java/com/example/carparkmanagement/CarparkmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.carparkmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarparkmanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarparkmanagementApplication.class, args);
+    }
+
+}
